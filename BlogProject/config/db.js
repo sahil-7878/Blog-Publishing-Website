ddfd53b53466221c2d1b8af25cf3ef24704327c2
Mr Saahil <mrsahil8316@gmail.com>
2026-07-10 22:40:59 +0530

    Update database connection to use environment variable

--- BlogProject/config/db.js
+++ BlogProject/config/db.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/node");
-        console.log("Database Connected Successfully");
-    } catch (error) {
-        console.error("Database Connection Error:", error.message);
-    }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Database Connected Successfully");
+  } catch (error) {
+    console.log("Database Connection Error:", error.message);
+  }
 };
 
 module.exports = connectDB;
