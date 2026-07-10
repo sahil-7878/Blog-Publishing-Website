b27ef7fd65c89d5d4db03296d216d0d24283d0ef
Mr Saahil <mrsahil8316@gmail.com>
2026-07-10 23:27:02 +0530

    Update app.js

--- BlogProject/app.js
+++ BlogProject/app.js
@@ -39,5 +39,5 @@ app.get("/", (req, res) => {
 
 // Start Server
 app.listen(PORT, () => {
-  console.log(Server is running on port ${PORT});
+  console.log('Server is running on port ${PORT}');
 });
